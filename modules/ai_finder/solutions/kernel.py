6332192c7b2160598bb41caf450d7be1a0322fd0
kokiez <105941365+kokiez@users.noreply.github.com>
2022-11-19 03:29:41 +0400

    updated modelhub and objects yaml links

--- modules/ai_finder/solutions/kernel.py
+++ modules/ai_finder/solutions/kernel.py
@@ -63,7 +63,7 @@ class Memory:
 
 
 class Assets:
-    GITHUB_RELEASE_API = "https://api.github.com/repos/qin2dim/hcaptcha-challenger/releases"
+    GITHUB_RELEASE_API = "https://api.github.com/repos/rofaya/hcaptcha-challenger/releases"
 
     NAME_ASSETS = "assets"
     NAME_ASSET_NAME = "name"
@@ -156,7 +156,7 @@ class Assets:
 
 class PluggableObjects:
     URL_REMOTE_OBJECTS = (
-        "https://raw.githubusercontent.com/QIN2DIM/hcaptcha-challenger/main/src/objects.yaml"
+        "https://raw.githubusercontent.com/rofaya/hcaptcha-challenger/main/src/objects.yaml"
     )
 
     DEFAULT_FILENAME = "objects.yaml"
